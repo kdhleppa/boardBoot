4b3a00c82e2fcc132547cf981cd5dde5f4cb3bd2
kdhleppa <newkdh8624@gmail.com>
2023-11-15 10:35:00 +0900

    1

---
+++ src/main/java/edu/kh/project/common/config/InterceptorConfig.java
@@ -0,0 +1,38 @@
+package edu.kh.project.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import edu.kh.project.common.interceptor.BoardTypeInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public BoardTypeInterceptor boardTypeInterceptor() {
+		return new BoardTypeInterceptor();
+	}
+	/*
+	@Bean
+	public BoardTypeInterceptor 다른인터셉터1() {
+		return new 다른인터셉터1();
+	}
+	*/
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(boardTypeInterceptor())
+		.addPathPatterns("/**") // 가로챌 경로 지정(여러개 작성시, 로 구분)
+		.excludePathPatterns("/css/**", "/images/**","/js/**");//가로채지않을경로
+
+		/*
+		registry.addInterceptor(다른인터셉터1())
+		.addPathPatterns("/**") // 가로챌 경로 지정(여러개 작성시, 로 구분)
+		.excludePathPatterns("/css/**", "/images/**","/js/**");//가로채지않을경로
+		*/
+	}
+
+}
